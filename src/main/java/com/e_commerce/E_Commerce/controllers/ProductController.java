23f60d7447bb8bec5d61c93a97827c7ce64d3388
satendrcoder <satendracaria@gmail.com>
2025-03-05 16:36:44 +0530

    Create Product API Method is completed

--- src/main/java/com/e_commerce/E_Commerce/controllers/ProductController.java
+++ src/main/java/com/e_commerce/E_Commerce/controllers/ProductController.java
@@ -1,14 +1,24 @@
 package com.e_commerce.E_Commerce.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.e_commerce.E_Commerce.dto.ProductDTO;
+import com.e_commerce.E_Commerce.services.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
+@AllArgsConstructor
 public class ProductController {
 
+    private ProductService productService;
     //@_getProduct
     //@_createProduct
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO){
+        return new ResponseEntity<>(productService.createPrduct(productDTO), HttpStatus.CREATED);
+    }
     //@update product
     //@delete product
     //@get product by id
--- src/main/java/com/e_commerce/E_Commerce/services/ProductService.java
+++ src/main/java/com/e_commerce/E_Commerce/services/ProductService.java
@@ -1,9 +1,33 @@
 package com.e_commerce.E_Commerce.services;
 
+import com.e_commerce.E_Commerce.dto.ProductDTO;
+import com.e_commerce.E_Commerce.entities.CategoryEntity;
+import com.e_commerce.E_Commerce.entities.ProductEntity;
+import com.e_commerce.E_Commerce.mapper.ProductMapper;
+import com.e_commerce.E_Commerce.repositories.CategoryRepository;
 import com.e_commerce.E_Commerce.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ProductService {
 
+    private ProductRepository productRepository;
+    private CategoryRepository categoryRepository;
+
+    public ProductDTO createPrduct(ProductDTO productDTO){
+       /**
+        * id, name, des, price, categoryId
+        **/
+       //Check Category Id
+       CategoryEntity category = categoryRepository.findById(productDTO.getCategoryId())
+                .orElseThrow(()-> new RuntimeException("Category not found!"));
+            // DTO -> Entity
+       ProductEntity product = ProductMapper.toproductEntity(productDTO, category);
+        product = productRepository.save(product);
+
+        // Entity to DTO
+        return ProductMapper.toproductDTO(product);
+    }
 }
